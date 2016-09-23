54dd0710e8d6a9fed8533f46d035d2f5754b43e2
Hidenori FUJIMURA <hfu@si.local>
2016-09-24 06:16:19 +0900

    update

--- forks.rb
+++ forks.rb
@@ -1,19 +1,21 @@
 require 'open-uri'
 require 'json'
 
-$user = 'gsi-cyberjapan'
-$repo = 'gsimaps'
-
-json = JSON.parse(open(
-  "https://api.github.com/repos/#{$user}/#{$repo}").read)
-forks_count = json['forks_count']
-print "#{$repo} has #{forks_count} forks.\n"
-
-(forks_count / 30.0).ceil.times {|page|
+def jump_into(user, repo)
   json = JSON.parse(open(
-    "https://api.github.com/repos/#{$user}/#{$repo}/forks" +
-    "?page=#{page + 1}").read)
-  json.each {|r|
-    print "#{r['owner']['login']}: #{r['html_url']}\n"
+    "https://api.github.com/repos/#{user}/#{repo}").read)
+  forks_count = json['forks_count']
+  print "#{user}/#{repo} has #{forks_count} forks.\n"
+
+  (forks_count / 30.0).ceil.times {|page|
+    json = JSON.parse(open(
+      "https://api.github.com/repos/#{user}/#{repo}/forks" +
+      "?page=#{page + 1}").read)
+    json.each {|r|
+      #jump_into(r['owner']['login'], repo)
+      print "#{r['owner']['login']}\n"
+    }
   }
-}
+end
+
+jump_into('gsi-cyberjapan', 'gsimaps')
